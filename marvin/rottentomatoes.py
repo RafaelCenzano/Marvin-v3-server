b108b48f132514bf836b6019f98efe8146f42df4
Savagecoder77 <savagecoder77@gmail.com>
2018-10-27 14:45:17 -0700

    clean up file and change request

--- marvin/rottentomatoes.py
+++ marvin/rottentomatoes.py
@@ -36,8 +36,7 @@ class TomatoeScrape:
             else:
                 raise Exception
         except Exception as e:
-            movie_data = {'type':'none',
-                          'score':'none','raiting':'none'}
+            movie_data = 400
             return movie_data
 
     def IMDb(self):
@@ -48,37 +47,5 @@ class TomatoeScrape:
             movie_data = {'raiting':up_pg.rstrip()}
             return movie_data
         except Exception as e:
-            movie_data = {'raiting':'none'}
+            movie_data = 400
             return movie_data
-
-class DefinitionFind:
-    def __init__(self, query):
-        self.query = query
-        url = ('https://www.dictionary.com/browse/' + self.query + '?s=t')# combine url with search query from command
-        r = get(url) # request page
-        page = r.text # formatting
-        self.soup = bs(page, 'html.parser') # parse html
-
-    def scrapeDefinition(self):
-        try:
-            define_find_type = self.soup.findAll('span', attrs={'class':'luna-pos'})
-            define_find = self.soup.findAll('span', attrs={'class':'css-9sn2pa e10vl5dg6'})
-            if define_find == []: raise Exception
-            if define_find_type == []: raise Exception
-            definition_type = define_find_type[0].getText()
-            definition = define_find[0].getText()
-            definition_type_corrected = self.removeComma(definition_type)
-            dictionary_data = {'type':definition_type_corrected,'definition':definition}
-            return dictionary_data
-        except Exception as e:
-            dictionary_data = {'type':'none','definition':'none'}
-            return dictionary_data
-
-    def removeComma(self, definition_type):
-        definition_type_letters = list(definition_type)
-        if ',' in definition_type_letters:
-            definition_type_letters.remove(',')
-            joined_definition_type = ("").join(definition_type_letters) # joining anything that was split from after any unnecessary words
-            return joined_definition_type
-        else:
-            return definition_type
