04c95fdc891c0d7dfead15c6c234ac28a4f2e39f
Savagecoder77 <savagecoder77@gmail.com>
2018-11-06 19:13:25 -0800

    change local url

--- marvin/database.py
+++ marvin/database.py
@@ -5,10 +5,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:////tmp/test.db', convert_unicode=True)
+engine = create_engine('postgresql://localhost:5432', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
+
 Base = declarative_base()
 Base.query = db_session.query_property()
 
