98cbb0244d17b98b2b89919b65aaf3d60445d49b
Savagecoder77 <savagecoder77@gmail.com>
2018-11-07 20:33:16 -0800

    moved new code from models.py and replace old code

--- marvin/database.py
+++ marvin/database.py
@@ -1,21 +1,19 @@
 # Imports
 
 # SQLAlchemy imports
-from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('postgresql://localhost:5432', convert_unicode=True)
-db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
+# A base model for other database tables to inherit
+class Base(db.Model):
+    __abstract__ = True
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    modified_at = db.Column(db.DateTime, default=db.func.current_timestamp(),
+                            onupdate=db.func.current_timestamp())
 
-Base = declarative_base()
-Base.query = db_session.query_property()
-
-def init_db():
-    # import all modules here that might define models so that
-    # they will be registered properly on the metadata.  Otherwise
-    # you will have to import them first before calling init_db()
-    import marvin.models
-    Base.metadata.create_all(bind=engine)
+roles_users = db.Table('roles_users',
+                       db.Column('user_id', db.Integer(),
+                                 db.ForeignKey('auth_user.id')),
+                       db.Column('role_id', db.Integer(),
+                                 db.ForeignKey('auth_role.id')))
