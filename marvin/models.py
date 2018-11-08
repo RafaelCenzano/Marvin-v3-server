5ba134060e64d390ecd2c823ef698a19167cf91e
Savagecoder77 <savagecoder77@gmail.com>
2018-11-07 20:48:22 -0800

    remove uneeded imports

--- marvin/models.py
+++ marvin/models.py
@@ -1,9 +1,8 @@
 # Imports
 from marvin.database import Base, roles_users
 from flask_security import UserMixin, RoleMixin
-from sqlalchemy import create_engine
 from sqlalchemy.orm import relationship, backref
-from sqlalchemy import Boolean, DateTime, Column, Integer, String, ForeignKey
+from sqlalchemy import Boolean, DateTime, Column, Integer, String
 
 class Role(Base, RoleMixin):
     __tablename__ = 'auth_role'
@@ -16,7 +15,6 @@ class Role(Base, RoleMixin):
     def __repr__(self):
         return '<Role %r>' % self.name
 
-
 class User(Base, UserMixin):
     __tablename__ = 'auth_user'
     email = db.Column(db.String(255), nullable=False, unique=True)
@@ -27,8 +25,6 @@ class User(Base, UserMixin):
     confirmed_at = db.Column(db.DateTime())
     last_login_at = db.Column(db.DateTime())
     current_login_at = db.Column(db.DateTime())
-    # Why 45 characters for IP Address ?
-    # See http://stackoverflow.com/questions/166132/maximum-length-of-the-textual-representation-of-an-ipv6-address/166157#166157
     last_login_ip = db.Column(db.String(45))
     current_login_ip = db.Column(db.String(45))
     login_count = db.Column(db.Integer)
