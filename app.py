0d134081b94069b7796aebd4277be9fc2c168cc7
Savagecoder77 <savagecoder77@gmail.com>
2018-11-07 20:49:14 -0800

    change imports and move models and database code to app.py for testing

--- app.py
+++ app.py
@@ -4,14 +4,14 @@
 from marvin.rottentomatoes import TomatoeScrape
 from marvin.define import DefinitionFind
 from marvin.youtube import YoutubeScrape
-#from marvin.database import db_session, init_db
-from marvin.models import User, Role, Base
+#from marvin.database import Base # db_session, init_db
+#from marvin.models import User, Role
 import config
 
 # Flask and extensions imports
 from flask import Flask, jsonify, request, render_template # Flask module
 from flask_sqlalchemy import SQLAlchemy # SQLAlchemy for database work
-from flask_security import Security, SQLAlchemySessionUserDatastore, auth_token_required # Security modules
+from flask_security import Security, SQLAlchemyUserDatastore, auth_token_required, http_auth_required # Security modules
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -21,7 +21,55 @@ app.config['SECRET_KEY'] = config.key
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore)
 
-# Create a user to test with
+# TESTING CODE
+
+class Base(db.Model):
+    __abstract__ = True
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    modified_at = db.Column(db.DateTime, default=db.func.current_timestamp(),
+                            onupdate=db.func.current_timestamp())
+
+roles_users = db.Table('roles_users',
+                       db.Column('user_id', db.Integer(),
+                                 db.ForeignKey('auth_user.id')),
+                       db.Column('role_id', db.Integer(),
+                                 db.ForeignKey('auth_role.id')))
+
+
+class Role(Base, RoleMixin):
+    __tablename__ = 'auth_role'
+    name = db.Column(db.String(80), nullable=False, unique=True)
+    description = db.Column(db.String(255))
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(Base, UserMixin):
+    __tablename__ = 'auth_user'
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    last_login_at = db.Column(db.DateTime())
+    current_login_at = db.Column(db.DateTime())
+    last_login_ip = db.Column(db.String(45))
+    current_login_ip = db.Column(db.String(45))
+    login_count = db.Column(db.Integer)
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+
+    def __repr__(self):
+        return '<User %r>' % self.email
+
+
+
+# Create a user to test
 @app.before_first_request
 def create_user():
     db.create_all()
@@ -35,7 +83,8 @@ Server for Marvin Virtual Assistant to improve functionality
 
 # TEST ROUTE
 @app.route('/dummy-api/', methods=['GET'])
-@auth_token_required
+#@auth_token_required
+@http_auth_required
 def dummyAPI():
     ret_dict = {
         "Key1": "Value1",
