eb554114adb86c4dba949362f4e6b5747e397200
Savagecoder77 <savagecoder77@gmail.com>
2018-11-09 20:30:30 -0800

    add database code

--- run.py
+++ run.py
@@ -3,15 +3,32 @@
 # Flask imports
 from flask import Flask
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+
+# Marvin Imports
+import config
 
 # Flask app
 app = Flask(__name__)
 
-# Create API
+# Configure app properties
+app.config['SECRET_KEY'] = config.key
+app.config['SQLALCHEMY_DATABASE_URI'] = config.database
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Create API property
 api = Api(app)
+
+# Create DB connection
+db = SQLAlchemy(app)
 
 # Import server modules
 import views, models, resources
+
+# Create tables
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 # Add routes
 api.add_resource(resources.UserRegistration, '/registration')
