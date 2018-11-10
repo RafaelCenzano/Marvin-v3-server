73c164d456d6f6be6048b3915be148aeee4201a0
Savagecoder77 <savagecoder77@gmail.com>
2018-11-09 20:39:36 -0800

    add class method

--- models.py
+++ models.py
@@ -10,3 +10,7 @@ class UserModel(db.Model):
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
+
+    @classmethod
+    def find_by_username(cls, username):
+        return cls.query.filter_by(username = username).first()
