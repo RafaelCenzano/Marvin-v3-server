6b7863c0c3d6b751e124bd37e39993c2858ed765
Savagecoder77 <savagecoder77@gmail.com>
2018-11-09 19:30:18 -0800

    add API and routes

--- run.py
+++ run.py
@@ -2,9 +2,22 @@
 
 # Flask imports
 from flask import Flask
+from flask_restful import Api
 
 # Flask app
 app = Flask(__name__)
 
+# Create API
+api = Api(app)
+
 # Import server modules
 import views, models, resources
+
+# Add routes
+api.add_resource(resources.UserRegistration, '/registration')
+api.add_resource(resources.UserLogin, '/login')
+api.add_resource(resources.UserLogoutAccess, '/logout/access')
+api.add_resource(resources.UserLogoutRefresh, '/logout/refresh')
+api.add_resource(resources.TokenRefresh, '/token/refresh')
+api.add_resource(resources.AllUsers, '/users')
+api.add_resource(resources.SecretResource, '/secret')
