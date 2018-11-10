4222b5e92151922aca2c18f2cadd6e8228059461
Savagecoder77 <savagecoder77@gmail.com>
2018-11-09 19:33:28 -0800

    add login required

--- resources.py
+++ resources.py
@@ -8,39 +8,47 @@ parser.add_argument('password', help = 'This field cannot be blank', required = 
 
 class UserRegistration(Resource):
     def post(self):
+        data = parser.parse_args()
         return {'message': 'User registration'}
 
 
 class UserLogin(Resource):
     def post(self):
+        data = parser.parse_args()
         return {'message': 'User login'}
 
 
 class UserLogoutAccess(Resource):
     def post(self):
+        data = parser.parse_args()
         return {'message': 'User logout'}
 
 
 class UserLogoutRefresh(Resource):
     def post(self):
+        data = parser.parse_args()
         return {'message': 'User logout'}
 
 
 class TokenRefresh(Resource):
     def post(self):
+        data = parser.parse_args()
         return {'message': 'Token refresh'}
 
 
 class AllUsers(Resource):
     def get(self):
+        data = parser.parse_args()
         return {'message': 'List of users'}
 
     def delete(self):
+        data = parser.parse_args()
         return {'message': 'Delete all users'}
 
 
 class SecretResource(Resource):
     def get(self):
+        data = parser.parse_args()
         return {
             'answer': 42
         }
