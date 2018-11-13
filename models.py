8fc937190dea95b469652eeb1b66eb382e94a120
Savagecoder77 <savagecoder77@gmail.com>
2018-11-12 18:58:03 -0800

    add class to revoke tokens

--- models.py
+++ models.py
@@ -32,3 +32,17 @@ class UserModel(db.Model):
     @staticmethod
     def verify_hash(password, hash):
         return sha256.verify(password + config.SECURITY_PASSWORD_SALT, hash)
+
+class RevokedTokenModel(db.Model):
+    __tablename__ = 'revoked_tokens'
+    id = db.Column(db.Integer, primary_key = True)
+    jti = db.Column(db.String(120))
+
+    def add(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def is_jti_blacklisted(cls, jti):
+        query = cls.query.filter_by(jti = jti).first()
+        return bool(query)
