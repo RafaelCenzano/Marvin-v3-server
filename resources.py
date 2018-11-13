8dcb0b8b5fb9e0896d8ec112543caf6a75c88e0d
Savagecoder77 <savagecoder77@gmail.com>
2018-11-12 18:43:18 -0800

    add jwt functions for tokens

--- resources.py
+++ resources.py
@@ -1,5 +1,6 @@
 # Flask Imports
 from flask_restful import Resource, reqparse
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt
 
 # Model Imports
 from models import UserModel
@@ -27,9 +28,13 @@ class UserRegistration(Resource):
         # Create user in db
         try:
             new_user.save_to_db()
+            access_token = create_access_token(identity = data['username'])
+            refresh_token = create_refresh_token(identity = data['username'])
             return {
-                'message': 'User {} was created'.format( data['username'])
-            }
+                'message': 'User {} was created'.format(data['username']),
+                'access_token': access_token,
+                'refresh_token': refresh_token
+                }
         except:
             return {'message': 'Something went wrong'}, 500
 
@@ -41,7 +46,7 @@ class UserLogin(Resource):
         if not current_user:
             return {'message': 'User {} doesn\'t exist'.format(data['username'])}
 
-        if data['password'] == current_user.password:
+        if UserModel.verify_hash(data['password'], current_user.password):
             return {'message': 'Logged in as {}'.format(current_user.username)}
         else:
             return {'message': 'Wrong credentials'}
