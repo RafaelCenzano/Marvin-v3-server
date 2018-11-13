07040577e6dcd1a2cde9bb21aa6534340406c610
Savagecoder77 <savagecoder77@gmail.com>
2018-11-12 18:56:44 -0800

    add refresh ability

--- resources.py
+++ resources.py
@@ -71,9 +71,11 @@ class UserLogoutRefresh(Resource):
 
 
 class TokenRefresh(Resource):
+    @jwt_refresh_token_required
     def post(self):
-        data = parser.parse_args()
-        return {'message': 'Token refresh'}
+        current_user = get_jwt_identity()
+        access_token = create_access_token(identity = current_user)
+        return {'access_token': access_token}
 
 
 class SecretResource(Resource):
