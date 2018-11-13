7bc3e7c6ed484c8a5e80e269844eb7cebc97a42f
Savagecoder77 <savagecoder77@gmail.com>
2018-11-12 16:20:10 -0800

    add comments

--- models.py
+++ models.py
@@ -1,16 +1,19 @@
 from run import db
 
 class UserModel(db.Model):
+    # Create new user in table
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(120), unique = True, nullable = False)
     password = db.Column(db.String(120), nullable = False)
 
+    # Add new user
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
 
+    # Check if user exists in database
     @classmethod
     def find_by_username(cls, username):
         return cls.query.filter_by(username = username).first()
