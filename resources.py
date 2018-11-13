f83ae33516988137e32db9ee9f07f8d4b3636075
Savagecoder77 <savagecoder77@gmail.com>
2018-11-12 19:15:05 -0800

    add revoke token

--- resources.py
+++ resources.py
@@ -3,7 +3,7 @@ from flask_restful import Resource, reqparse
 from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt
 
 # Model Imports
-from models import UserModel
+from models import UserModel, RevokedTokenModel
 
 # Parse incoming data
 parser = reqparse.RequestParser()
