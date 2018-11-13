8b469a9f91bf08cf27c5306849b62fab85deac59
Savagecoder77 <savagecoder77@gmail.com>
2018-11-12 18:33:37 -0800

    add new static methods for hashing

--- models.py
+++ models.py
@@ -22,3 +22,11 @@ class UserModel(db.Model):
     @classmethod
     def find_by_username(cls, username):
         return cls.query.filter_by(username = username).first()
+
+    @staticmethod
+    def generate_hash(password):
+        return sha256.hash(password)
+
+    @staticmethod
+    def verify_hash(password, hash):
+        return sha256.verify(password, hash)
