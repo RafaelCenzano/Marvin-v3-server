eda8b03328985c0f6b907690bd1977026195f548
Savagecoder77 <savagecoder77@gmail.com>
2018-11-13 21:29:41 -0800

    add new comments

--- marvin/youtube.py
+++ marvin/youtube.py
@@ -1,41 +1,41 @@
 #Imports
-from bs4 import BeautifulSoup as bs # process html
-from requests import get # to request page url code
-from urllib.parse import urlparse # urlparse to parse url data
+from bs4 import BeautifulSoup as bs
+from requests import get
+from urllib.parse import urlparse
 
 
 '''
 File that was created to help my lazy self
-open youtube without my voice
+open youtube with my voice
 
-Webscraper that gets the first 2 non Advertisements
+Webscraper that gets the first 2 non advertisement
 videos that show up when we it searches for your query
 '''
 
 # Class for all youtube based scraping
 class YoutubeScrape:
-    def __init__(self, query): # pass in search query
+    def __init__(self, query):
 
         # create self variables to hold class constants
         self.query = query
-        self.videolist = [] # create empty list
-        self.url = ('https://www.youtube.com/results?search_query=' + self.query)# combine url with search query from command
+        self.videolist = []
+        self.url = ('https://www.youtube.com/results?search_query=' + self.query)
 
         # request data section
-        r = get(self.url) # request page
-        page = r.text # formatting
+        r = get(self.url)
+        page = r.text
 
         # start soup functions
-        soup = bs(page, 'html.parser') # parse html
-        self.vids = soup.findAll(attrs={'class':'yt-uix-tile-link'}) # search for class yt-uix-tile-link in html from page
+        soup = bs(page, 'html.parser')
+        self.vids = soup.findAll(attrs={'class':'yt-uix-tile-link'})
 
     def is_absolute(self, url):
-        # determine if data is already a url
+        # determine if input url is true url
         return bool(urlparse(url).netloc)
 
     def scrapeYoutube(self):
         # go through all found links with
-        for v in self.vids: #for loop for finding all videos that show up
+        for v in self.vids:
 
             # if link that its checking is already a true link then it is an ad
             if self.is_absolute(v['href']) == True:
@@ -43,8 +43,8 @@ class YoutubeScrape:
 
             # when not true link so its a real video
             else:
-                tmp = 'https://www.youtube.com' + v['href'] # create url to add to list with links from html
-                self.videolist.append(tmp) # add the newly created url to list
+                tmp = 'https://www.youtube.com' + v['href']
+                self.videolist.append(tmp)
 
         # create dictionary to return to send found data in json format
         video_link = {'link':self.videolist[0],'link2':self.videolist[1]}
